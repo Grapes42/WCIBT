5249257df98b6af33a6bd20b980e9a31f1a2871f
Max <f.inggrapes@gmail.com>
2021-08-11 18:48:59 +1200

    First commit. Made dish picker main.cpp

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	
+	string dishes[] = {"Beurre blanc",
+    "Beurre fondue",
+    "Beurre Maître d'Hôtel",
+    "Beurre manié",
+    "Beurre monté",
+    "Beurre noir",
+    "Beurre noisette",
+    "Bread and butter pudding",
+    "Buttered rice",
+    "Butter cake",
+    "Butter cookie",
+    "Butter pecan",
+    "Butter pie",
+    "Butter tart",
+    "Butter tea",
+    "Butterbrot",
+    "Buttercream",
+    "Butterkuchen",
+    "Buttermilk pie",
+    "Butterscotch",
+    "Buttery (bread)",
+    "Chicken Kiev",
+    "Chocolate butter",
+    "Compound butter, or beurre composé",
+    "Cookie butter",
+    "Croissant",
+    "Danish pastry",
+    "Deep-fried butter",
+    "Egg butter",
+    "Garlic butter, or beurre à la bourguignonne",
+    "Gooey butter cake",
+    "Hard sauce",
+    "Hollandaise sauce",
+    "Karelian pasty",
+    "Kouign-amann",
+    "Linzer torte",
+    "Pain aux raisins",
+    "Pozharsky cutlet",
+    "Popcorn",
+    "Puff pastry",
+    "Remonce",
+    "Torpedo dessert"};
+    
+    
+    size_t count = 0;
+    for (size_t i = 0; i < sizeof(dishes)/sizeof(*dishes); i++)
+        if (dishes[i] != "")
+            count++;
+    //cout << count << endl;
+    
+    cout << dishes[rand() % count-1 + 0] << endl;
+    
+    return 0;
+}
